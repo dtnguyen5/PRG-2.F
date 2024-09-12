ac026607fe14e0857ff51dd459940c32710572b2
Honza <duy.nguyen@infis.cz>
2024-09-12 15:24:29 +0200

    Update Helloworld.java

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -1,5 +1,16 @@
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Je mi to jedno něco tady bude");
+        System.out.println("Je mi to jedno něco tady bude");
+        System.out.println("Je mi to jedno něco tady bude");
+        System.out.println("Je mi to jedno něco tady bude");
+        System.out.println("Je mi to jedno něco tady bude");
+        System.out.println("Je mi to jedno něco tady bude");
+        System.out.println("Je mi to jedno něco tady bude");
+        System.out.println("Je mi to jedno něco tady bude");
+        System.out.println("Je mi to jedno něco tady bude");
+        System.out.println("Je mi to jedno něco tady bude");
+        System.out.println("Je mi to jedno něco tady bude");
+        System.out.println("Je mi to jedno něco tady bude");
     }
 }
