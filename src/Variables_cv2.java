1e3da47ec74a2949d58cbb2954f7624d0cff5bd7
Honza <duy.nguyen@infis.cz>
2024-09-30 19:21:06 +0200

    Changes 4

---
+++ src/Variables_cv2.java
@@ -0,0 +1,20 @@
+public class Variables_cv2 {
+    public static void main(String[] args) {
+        /*
+         * CV 2
+         * Napište jednoduchou kalkulačku, která bude umět +, -, *, / nad dvěma čísly z množiny R (Reálných čísel).
+         * Výsledky vypište na konzoli
+         */
+
+        double num1 = 12;
+        double num2 = 20;
+
+        double some = num1 + num2; //int
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+
+    }
+}
