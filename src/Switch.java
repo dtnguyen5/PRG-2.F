dcdeab36a185c3895ae8828d8ffc763765637538
Honza <duy.nguyen@infis.cz>
2024-10-07 13:11:07 +0200

    Update Switch.java

--- src/Switch.java
+++ src/Switch.java
@@ -1,1 +1,26 @@
+public class Switch {
+    public static void main(String[] args) {
+
+        int a = 8;
+
+        switch (a){
+            case 5:
+                System.out.println("je sobota");
+                break;
+            case 6:
+                System.out.println("je neděle");
+                break;
+            default:
+                System.out.println("Je pracovní týden");
+                break;
+
+
+
+        }
+        // ctrl + alt + l
+
+
+
+    }
+}
 
