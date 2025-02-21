6d11bcb1d0d9b6960a8fa18bb42483d40b1c33a9
Honza <duy.nguyen@infis.cz>
2025-02-21 19:20:42 +0100

    trideni pol e

--- src/Únor/trideni_pole.java
+++ src/Únor/trideni_pole.java
@@ -1,21 +1,32 @@
 package Únor;
 
+import java.lang.reflect.Array;
+
 public class trideni_pole {
     public static void main(String[] args) {
 
         // Bubblesort - porovnávání pak se prohodí
 
         String[] data = {"Bohdan", "Lucka", "Jakub", "Pavel"};
-        int[] vysky = {185, 185, 190, 195};
+        int[] vysky = {200, 195, 190, 185};
 
-        for (int i = 0; i < vysky.length - 1 ; i++) {
+        for (int j = 0; j < vysky.length; j++) {
 
-            if (vysky[0] > vysky[1]){
-                int temp = vysky[0]; // 180
-                vysky[0] = vysky[i + 1];
-                vysky[ i + 1] = temp;
+            for (int i = 0; i < vysky.length - 1 ; i++) {
+
+                if (vysky[0] > vysky[1]){
+                    int temp = vysky[0]; // 180
+                    vysky[0] = vysky[i + 1];
+                    vysky[ i + 1] = temp;
+                }
             }
-
         }
+
+        // Array.sort(vysky); bude rychlejsi nez to co jsem zrobil
+
+        for (int cislo : vysky) {
+            System.out.println(cislo + "");
+        }
+
     }
 }
