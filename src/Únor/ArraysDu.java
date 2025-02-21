43c75953960926ec4a30d4129107693c3ad536a6
Honza <duy.nguyen@infis.cz>
2025-02-21 23:02:39 +0100

    Nejtezsi ukol co jsem kdy zrobil

--- src/Únor/ArraysDu.java
+++ src/Únor/ArraysDu.java
@@ -5,16 +5,38 @@ import java.util.Arrays;
 public class ArraysDu {
 
     static String[] inventar = {
-            "Spitzhacke", "Krankenwagen", "Apache-Hubschrauber 3000", "Monchhichi",
-            "Chihuahua", "Gallone Milch", "Hubba Bubba", "48-Port-Switch",
-            "Stiefel", "Schwanz eines weißen Wals", "DVD-Player", "16GB RAM",
-            "LGM 30 Minuteman", "Lego Ninja Go", "Excalibur", "50-Meter-Seil",
-            "Quanten-Discombobulator", "Empfindungsfähiges Gummiente",
-            "Unsichtiges Glücks-Frettchen", "Zeitreisender Hamster", "Kosmische Luftpolsterfolie",
-            "Telepathischer Spatel", "Interdimensionales Käserad", "Verzerrte Lavalampe",
-            "Tanzender Kaktus des Chaos", "Mystische Fleischkugel", "Einhornfürze im Glas",
-            "Singender Weltraum-Taco", "Antischwerkraft-Banane", "Multiversum-Becher",
-            "Hellseherischer Toaster", "Atom-Schaumbad"
+            "Spitzhacke",
+            "Krankenwagen",
+            "Apache-Hubschrauber 3000",
+            "Monchhichi",
+            "Chihuahua",
+            "Gallone Milch",
+            "Hubba Bubba",
+            "48-Port-Switch",
+            "Stiefel",
+            "Schwanz eines weißen Wals",
+            "DVD-Player",
+            "16GB RAM",
+            "LGM 30 Minuteman",
+            "Lego Ninja Go",
+            "Excalibur",
+            "50-Meter-Seil",
+            "Quanten-Discombobulator",
+            "Empfindungsfähiges Gummiente",
+            "Unsichtiges Glücks-Frettchen",
+            "Zeitreisender Hamster",
+            "Kosmische Luftpolsterfolie",
+            "Telepathischer Spatel",
+            "Interdimensionales Käserad",
+            "Verzerrte Lavalampe",
+            "Tanzender Kaktus des Chaos",
+            "Mystische Fleischkugel",
+            "Einhornfürze im Glas",
+            "Singender Weltraum-Taco",
+            "Antischwerkraft-Banane",
+            "Multiversum-Becher",
+            "Hellseherischer Toaster",
+            "Atom-Schaumbad"
     };
 
     static char[] rarity = {
